219ad757c924138381994b5199e35e866314fa2e
xiaodong yang <xiaodongy86@gmail.com>
2019-08-28 10:45:14 -0400

    Create 01_FastQC.sh

---
+++ 01_FastQC.sh
@@ -0,0 +1,27 @@
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big1/big1_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big1/big1_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big2/big2_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big2/big2_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big3/big3_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big3/big3_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml1/sml1_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml1/sml1_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml2/sml2_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml2/sml2_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml3/sml3_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml3/sml3_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT1/WT1_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT1/WT1_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT2/WT2_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT2/WT2_2.fq.gz
+
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT3/WT3_1.fq.gz
+ts fastqc -t 4 fastq --outdir=/data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/FastQC/ /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT3/WT3_2.fq.gz
