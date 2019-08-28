56063b8ec85fcf02a06b46927accbfdf806a1a99
xiaodong yang <xiaodongy86@gmail.com>
2019-08-28 11:07:41 -0400

    Create 02_trim_galore.sh

---
+++ 02_trim_galore.sh
@@ -0,0 +1,14 @@
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/big1 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big1/big1_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big1/big1_2.fq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/big2 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big2/big2_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big2/big2_2.fq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/big3 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big3/big3_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/big3/big3_2.fq.gz
+
+
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/sml1 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml1/sml1_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml1/sml1_2.fq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/sml2 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml2/sml2_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml2/sml2_2.fq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/sml3 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml3/sml3_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/sml3/sml3_2.fq.gz
+
+
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/WT1 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT1/WT1_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT1/WT1_2.fq.gz 
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/WT2 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT2/WT2_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT2/WT2_2.fq.gz 
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/trim_galore/WT3 /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT3/WT3_1.fq.gz /data5/F19FTSUSAT0692_ARAluwE/cdts-hk.genomics.cn/Clean/WT3/WT3_2.fq.gz 
+
